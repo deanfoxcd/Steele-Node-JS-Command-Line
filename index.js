3eb121fc628ac9d141d5a37f5d71888b6cf32d0d
Dean Fox <deanfoxcd@gmail.com>
2024-10-29 19:49:43 -0400

    add colors and styles in console to separate files and folders and add directory support

--- index.js
+++ index.js
@@ -1,9 +1,38 @@
 #!/usr/bin/env node
 
-import { readdir } from 'node:fs';
+import { readdir, promises } from 'node:fs';
 import { cwd } from 'node:process';
+import chalk from 'chalk';
+import path from 'node:path';
 
-readdir(cwd(), (err, files) => {
+const { lstat } = promises;
+const targetDir = process.argv[2] || process.cwd();
+
+readdir(targetDir, async (err, files) => {
   if (err) throw new Error(err);
-  console.log(files);
+
+  const promises = files.map((file) => {
+    return lstat(path.join(targetDir, file));
+  });
+
+  const allStats = await Promise.all(promises);
+
+  for (let stat of allStats) {
+    const index = allStats.indexOf(stat);
+
+    // console.log(files[index], stat.isFile());
+
+    if (stat.isFile()) console.log(chalk.blue(files[index]));
+    else console.log(chalk.bold.red(files[index]));
+  }
+
+  //   for (let file of files) {
+  //     try {
+  //       const stats = await lstat(file);
+  //       console.log(file, stats.isFile());
+  //     } catch (err) {
+  //       console.error(err);
+  //     }
+  //   }
+  // Figuring out what is a file and what is a folder
 });
